b87d26ac644d04737189a67e2fd416cac2b48776
Eneias <medinasp@gmail.com>
2022-02-26 16:17:47 -0300

    First Commit

---
+++ Models/PaymentDetailContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext()
+        {
+        }
+
+
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=GuiaConstrucao;Integrated Security=True");
+//            }
+//        }
+    }
+}
